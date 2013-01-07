0f0556e4a2d3cb956dff04a0b04758133e3b7b05
Tele David Ogundeko <tele.ogundeko@ackuit.co.uk>
2013-01-07 06:41:43 +0100

    Shop Section
    Shop Section Added

---
+++ shop/includes/_doc_foot.php
@@ -0,0 +1,55 @@
+<!-- BOTTOM CONTENTS -->
+ <div id="BottomSections">
+ 	<div id="linksbar"></div>
+ 	<div id="bottom-bar"></div>
+ </div>
+ 
+  
+  <!-- Included JS Files (Uncompressed) -->
+  <!--
+  <script src="javascripts/jquery.js"></script><script src="javascripts/jquery.foundation.mediaQueryToggle.js"></script><script src="javascripts/jquery.foundation.forms.js"></script><script src="javascripts/jquery.foundation.reveal.js"></script><script src="javascripts/jquery.foundation.orbit.js"></script><script src="javascripts/jquery.foundation.navigation.js"></script><script src="javascripts/jquery.foundation.buttons.js"></script><script src="javascripts/jquery.foundation.tabs.js"></script><script src="javascripts/jquery.foundation.tooltips.js"></script><script src="javascripts/jquery.foundation.accordion.js"></script><script src="javascripts/jquery.placeholder.js"></script><script src="javascripts/jquery.foundation.alerts.js"></script><script src="javascripts/jquery.foundation.topbar.js"></script><script src="javascripts/jquery.foundation.joyride.js"></script><script src="javascripts/jquery.foundation.clearing.js"></script><script src="javascripts/jquery.foundation.magellan.js"></script>
+  -->
+  
+
+
+
+  
+  <!-- Showcase -->
+  <script type="text/javascript" src="../../includes/apps/showcase/js/slider.js"></script>
+		<script type="text/javascript">
+			$(function() {
+
+				Slider.init();
+			
+			});
+		</script>
+  
+  
+  <!-- Explore -->
+  <script type="text/javascript" src="../../includes/js/jquery.stackslider.js"></script>
+		<script type="text/javascript">
+			
+			$( function() {
+				
+				$( '#st-stack' ).stackslider();
+
+			});
+
+		</script>
+        
+        
+  <!--<script src="javascripts/foundation.min.js"></script>-->
+  
+   <!-- Initialize JS Plugins --> 
+  <script src="../../includes/javascripts/app.js"></script>
+  <script src="../../includes/javascripts/jquery.foundation.orbit.js"></script>
+
+  
+    <script>
+    $(window).load(function(){
+      $("#featured").orbit();
+    });
+    </script> 
+  
+</body>
+</html>
---
+++ shop/includes/_doc_head.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title><?php echo $sitetitle ?></title>
+  
+  <!-- Included CSS Files (Uncompressed) -->
+  
+  <link rel="stylesheet" href="../../stylesheets/foundation.css">
+  <link rel="stylesheet" href="../../stylesheets/presentation.css">
+  <link rel="stylesheet" href="../style/shop.css">
+  <link rel="stylesheet" href="../../css/style.css">
+  <link rel="stylesheet" href="../../stylesheets/fonts.css">
+  
+  <link rel="stylesheet" type="text/css" href="../../showcase/css/style.css" />
+  
+  
+  <!-- Included CSS Files (Compressed) -->
+  <!--<link rel="stylesheet" href="stylesheets/foundation.min.css">-->
+  <link rel="stylesheet" href="../../stylesheets/app.css">
+  
+  <!-- Typography -->
+  <link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Bitter:400,700,400italic' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Archivo+Narrow:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Dosis:200,400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  
+  <!-- Icons -->
+  <link rel="stylesheet" href="../../stylesheets/general_enclosed_foundicons.css">
+  <link rel="stylesheet" href="../../stylesheets/fc-webicons.css">
+
+  <script src="../../javascripts/modernizr.foundation.js"></script>
+  <!--[if lte IE 7]><style>.support-note .note-ie{display:block;}</style><![endif]-->
+  
+  
+  <!-- Included JS Files (Compressed) -->
+  <script src="../../javascripts/jquery.js"></script>
+  
+  <!-- Scroll to Navigation -->
+  <script src="../../javascripts/jquery.scrollTo-1.4.3.1-min.js"></script>
+  <script src="../../javascripts/jquery.localscroll.js"></script>
+  <script type="text/javascript">
+		$(document).ready(function() {
+   			$('#pagelinks').localScroll({duration:800});
+		});
+		
+		$(document).ready(function() {
+   			$('#nav').localScroll({duration:800});
+		});
+</script>
+
+
+</head>
+<body>
+ 
+ 
+ 
+ <!-- BEGIN TOP SECTIONS -->
+  
+  
+  
+  <div id="TopSections">
+  
+  	<div id="top-bar">        
+    	<div class="row">
+        	<section>
+            	<nav class="top-bar">
+                	<ul class="right">
+                    	
+                    	
+                        <li><a class="fc-webicon facebook" href="#">Facebook</a></li>
+                        <li>&nbsp;&nbsp;</li>
+                        <li><a class="fc-webicon twitter" href="#">Twitter</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </div>
+    </div>
+    
+    
+    <div id="navbar">
+  	<div class="row">
+    	<section>
+        	<nav>
+            	<ul id="pagelinks" class="nav-bar left">
+                	<li><a href="#"><span class="foundicon-home"></span>home</a></li>
+                    <li><a href="#exploresection"><span class="foundicon-search"></span>explore</a></li>
+                    <li><a href="#shopsection"><span class="foundicon-cart"></span>shop</a></li>
+                </ul>
+     <!--<ul style="list-style:none;" class="logo"><li><a href="#"><img src="images/newlogo.png"/></a></li></ul>-->
+                <ul id="nav" class="nav-bar right">
+                	<li><a href="#"><span class="foundicon-idea"></span>create</a></li>
+                    <li><a href="#aboutsection"><span class="foundicon-people"></span>about</a></li>
+                    <li><a href="#"><span class="foundicon-mic"></span>press</a></li>
+                </ul>
+            </nav>
+        </section>
+    </div>
+  </div>
+  <ul class="logo"><li><a href="#"><img src="../../images/newlogo.png"/></a></li></ul>
+  <div class="shopnavi"></div>
+    
+  </div>
+  
+  
+  <!-- END TOP SECTIONS -->
